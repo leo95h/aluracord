fc2ffc22dbc966b5a9f7b6c4d1579942aa1d9611
leo95h <leonardo.lhpr@gmail.com>
2022-01-25 15:59:31 -0300

    estrutura inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+//Component React
+function HomePage() {
+    //JSX
+    return (
+        <div>
+            <h1>Boas vindas de volta!</h1>
+            <h2>Discord - Alura Matrix</h2>
+        </div>
+    )
+}
+
+export default HomePage
